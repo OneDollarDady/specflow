08f55590a3b02693d0bc2a482712ac21688233db
Thomas BEAUDROUET <thomas.beaudrouet@euroinfo.int>
2020-05-08 13:13:47 +0200

    commit 2

---
+++ UnitTestProject1/SpecFlowFeature1.feature
@@ -0,0 +1,26 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add two numbers
+	Given I have entered 50 into the calculator
+	And I have entered 70 into the calculator
+	When I press add
+	Then the result shouldt be 120 on the screen
+
+
+@mytag
+Scenario: toujours vrai
+	Given I have entered 50 into the calculator
+	And I have entered 70 into the calculator
+	When I press add
+	Then toujours 2 vrai
+
+@mytag
+Scenario: toujours faux
+	Given I have entered 50 into the calculator
+	And I have entered 70 into the calculator
+	When I press add
+	Then toujours 2 faux
---
+++ UnitTestProject1/SpecFlowFeature1Steps.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1.obj
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        string TestPassageDataEntreStep;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            //ScenarioContext.Current.Pending();
+            TestPassageDataEntreStep = "valeur une";
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            if (TestPassageDataEntreStep == "valeur une")
+                TestPassageDataEntreStep = "valeur deux";
+            else
+                TestPassageDataEntreStep = "valeur trois";
+        }
+        
+        [Then(@"the result shouldt be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.IsTrue(TestPassageDataEntreStep == "valeur deux", "La valeur vaux: "+TestPassageDataEntreStep);
+        }
+
+        [Then(@"toujours (.*) vrai")]
+        public void ThenAlwaysTrue(int p0)
+        {
+            Assert.IsTrue(p0==2);
+        }
+
+        [Then(@"toujours (.*) faux")]
+        public void ThenAlwaysFalse(int p0)
+        {
+            Assert.IsTrue(false, "oups les carottes sont cuites");
+        }
+    }
+}
